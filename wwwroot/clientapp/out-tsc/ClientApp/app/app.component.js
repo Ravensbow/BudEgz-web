8a6413860605bbb3a1d2da0d67d66c2a62e404ad
Ravenbow <walkoskuba@gmail.com>
2019-03-19 13:36:53 +0100

    Dodanie Angulara

---
+++ wwwroot/clientapp/out-tsc/ClientApp/app/app.component.js
@@ -0,0 +1,17 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'Moja dupa';
+    }
+    AppComponent = tslib_1.__decorate([
+        Component({
+            selector: 'the-test',
+            template: "\n    <div style=\"text-align:center\">\n      <h1>\n        Welcome aaa {{title}}!\n      </h1>\n    </div>\n  ",
+            styles: []
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+export { AppComponent };
+//# sourceMappingURL=app.component.js.map
